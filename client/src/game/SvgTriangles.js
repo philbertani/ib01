8bd614a2d88fedac947f1c96893d0df86ee3eccb
Philip Bertani <philip.bertani@gmail.com>
2023-12-18 23:49:42 -0500

    using SVG for graphics for now, works nicely

---
+++ client/src/game/SvgTriangles.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+export function RightTriangleLowerLeft({ color }) {
+  //whatever is in the viewBox gets scaled to % of container - nice!
+  return (
+    <svg style={{ position: "absolute" }} width="100%" viewBox="0 0 200 200">
+      <polygon
+        points="0,0 0,200 200,200"
+        style={{ fill: color, stroke: "purple", strokeWidth: 1 }}
+      />
+    </svg>
+  );
+}
+
+export function RightTriangleUpperRight({ color }) {
+  return (
+    <svg style={{ position: "absolute" }} width="100%" viewBox="0 0 200 200">
+      <polygon
+        points="0,0 200,200 200,0"
+        style={{ fill: color, stroke: "purple", strokeWidth: 1 }}
+      />
+    </svg>
+  );
+}
+
+export function RightTriangleUpperLeft({ color }) {
+  return (
+    <svg style={{ position: "absolute" }} width="100%" viewBox="0 0 200 200">
+      <polygon
+        points="0,0 200,0 0,200"
+        style={{ fill: color, stroke: "purple", strokeWidth: 1 }}
+      />
+    </svg>
+  );
+}
+
+export function Left({ color }) {
+  return (
+    <svg style={{ position: "absolute" }} width="100%" viewBox="0 0 200 200">
+      <polygon
+        points="0,0 0,200 100,100"
+        style={{ fill: color, stroke: "purple", strokeWidth: 1 }}
+      />
+    </svg>
+  );
+}
+
+export function Right({ color }) {
+  return (
+    <svg style={{ position: "absolute" }} width="100%" viewBox="0 0 200 200">
+      <polygon
+        points="200,0 200,200 100,100"
+        style={{ fill: color, stroke: "purple", strokeWidth: 1 }}
+      />
+    </svg>
+  );
+}
+
+export function Up({ color }) {
+  return (
+    <svg style={{ position: "absolute" }} width="100%" viewBox="0 0 200 200">
+      <polygon
+        points="0,0 200,0 100,100"
+        style={{ fill: color, stroke: "purple", strokeWidth: 1 }}
+      />
+    </svg>
+  );
+}
+
+export function Down({ color }) {
+  return (
+    <svg style={{ position: "absolute" }} width="100%" viewBox="0 0 200 200">
+      <polygon
+        points="0,200 200,200 100,100"
+        style={{ fill: color, stroke: "purple", strokeWidth: 1 }}
+      />
+    </svg>
+  );
+}
+
--- client/src/game/TargetBoard.js
+++ client/src/game/TargetBoard.js
@@ -1,10 +1,30 @@
 import React from "react";
+import * as SVGTRI from "./SvgTriangles";
 import {useStore} from "react-redux";
 
 export default function TargetBoard({style}) {
   const styleFinal = {...style};
-  styleFinal.backgroundColor = "yellow";
+  styleFinal.backgroundColor = "rgba(255,255,255,0)";
 
-  return <div style={styleFinal}></div>
+  return <div style={styleFinal}>
+
+    <div style={{position:"absolute", width:styleFinal.width/5}}>
+      <SVGTRI.RightTriangleLowerLeft boardDims={styleFinal} color={"rgba(255,255,0,.5)"}/>
+      <SVGTRI.RightTriangleUpperRight boardDims={styleFinal} color={"rgba(0,255,255,.5)"}/>
+    </div>
+
+    <div style={{position:"absolute",width:styleFinal.width/5 , left:styleFinal.width/5}}>
+      <SVGTRI.RightTriangleLowerLeft boardDims={styleFinal} color={"rgba(255,255,0,.5)"}/>
+      <SVGTRI.RightTriangleUpperRight boardDims={styleFinal} color={"rgba(0,255,255,.5)"}/>
+    </div>
+
+    <div style={{position:"absolute",width:styleFinal.width/5 , top:styleFinal.width/5}}>
+      <SVGTRI.Left boardDims={styleFinal} color={"rgba(0,255,0,.5)"}/>
+      <SVGTRI.Right boardDims={styleFinal} color={"rgba(0,255,0,.5)"}/>
+      <SVGTRI.Up boardDims={styleFinal} color={"rgba(255,0,255,.5)"}/>
+      <SVGTRI.Down boardDims={styleFinal} color={"rgba(255,0,255,.5)"}/>
+    </div>
+
+  </div>
 }
 
